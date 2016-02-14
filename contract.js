3399b8637a445ca7ee1460832ca4c87dd64e79c3
segovro <segovro@gmail.com>
2016-02-14 18:08:16 -0500

    10currencies

--- contract.js
+++ contract.js
@@ -1,12 +1,9 @@
-contract usersKeys {
-	
+contract keysWallet {
+    // Here we store the systemIDs and their receive addresses at each currency. Make it public to automatically generate an
 
-	function usersKeys () {
-	}
-	
 	struct keys {
-		address ethAddress;
-		bytes32 eID;
+		bytes32	systemID;		
+		bytes32	ethereum;
 		bytes32	currency0;		
 		bytes32	currency1;
 		bytes32 currency2;
@@ -18,54 +15,53 @@ contract usersKeys {
 		bytes32	currency8;
 		bytes32	currency9;
 	}
+	
+    mapping (bytes32 => keys) public keysWallet;
 
-    mapping (address => keys) public keysWallet;
-
-    function register(bytes32 username) {
-        if(keysWallet[msg.sender].eID == "" && username != ""){
-           keysWallet[msg.sender].ethAddress = msg.sender;
-	   keysWallet[msg.sender].eID = username;
+    // Register the provided systemID with the caller address.
+    // Also, we don't want them to register "" as their systemID.
+    function register(bytes32 mySystemID) {
+        if(keysWallet[mySystemID].mySystemID == 0 && mySystemID != ""){
+            keysWallet[mySystemID].ethereum = msg.sender;
         }
     }
 
-    function unregister(bytes32 username) {
-	if(keysWallet[msg.sender].eID == username){
-            keysWallet[msg.sender].eID = "";
+    // Unregister the provided mySystemID with the caller address.
+    function unregister(bytes32 mySystemID) {
+        if(keysWallet[mySystemID].mySystemID != 0 && mySystemID != ""){
+            keysWallet[mySystemID].mySystemID = "";
         }
     }
-	
-	function registerKeyforCurrency(uint currencyNumber, bytes32 newKey) {
-		if(keysWallet[msg.sender].eID != 0){
-			if (currencyNumber == 0) {keysWallet[msg.sender].currency0 = newKey;}
-			if (currencyNumber == 1) {keysWallet[msg.sender].currency1 = newKey;}
-			if (currencyNumber == 2) {keysWallet[msg.sender].currency2 = newKey;}
-			if (currencyNumber == 3) {keysWallet[msg.sender].currency3 = newKey;}
-			if (currencyNumber == 4) {keysWallet[msg.sender].currency4 = newKey;}
-			if (currencyNumber == 5) {keysWallet[msg.sender].currency5 = newKey;}
-			if (currencyNumber == 6) {keysWallet[msg.sender].currency6 = newKey;}
-			if (currencyNumber == 7) {keysWallet[msg.sender].currency7 = newKey;}
-			if (currencyNumber == 8) {keysWallet[msg.sender].currency8 = newKey;}
-			if (currencyNumber == 9) {keysWallet[msg.sender].currency9 = newKey;}
+    
+	function registerKeyCurrency(bytes32 mySystemID, uint currencyNumber, bytes32 newKey) {
+		if(keysWallet[mySystemID].ethereum == msg.sender) {
+			if (currencyNumber == 0) {keysWallet[mySystemID].currency0 = newKey;}
+			if (currencyNumber == 1) {keysWallet[mySystemID].currency1 = newKey;}
+			if (currencyNumber == 2) {keysWallet[mySystemID].currency2 = newKey;}
+			if (currencyNumber == 3) {keysWallet[mySystemID].currency3 = newKey;}
+			if (currencyNumber == 4) {keysWallet[mySystemID].currency4 = newKey;}
+			if (currencyNumber == 5) {keysWallet[mySystemID].currency5 = newKey;}
+			if (currencyNumber == 6) {keysWallet[mySystemID].currency6 = newKey;}
+			if (currencyNumber == 7) {keysWallet[mySystemID].currency7 = newKey;}
+			if (currencyNumber == 8) {keysWallet[mySystemID].currency8 = newKey;}
+			if (currencyNumber == 9) {keysWallet[mySystemID].currency9 = newKey;}
 			}
 		}
 	
-	function getKeyforCurrencyforUser (bytes32 username, uint currencyNumber) returns (bytes32 myKey) {
-		if(keysWallet[msg.sender].eID != ""){
-			if (currencyNumber == 0) {myKey = keysWallet[msg.sender].currency0;}
-			if (currencyNumber == 1) {myKey = keysWallet[msg.sender].currency1;}
-			if (currencyNumber == 2) {myKey = keysWallet[msg.sender].currency2;}
-			if (currencyNumber == 3) {myKey = keysWallet[msg.sender].currency3;}
-			if (currencyNumber == 4) {myKey = keysWallet[msg.sender].currency4;}
-			if (currencyNumber == 5) {myKey = keysWallet[msg.sender].currency5;}
-			if (currencyNumber == 6) {myKey = keysWallet[msg.sender].currency6;}
-			if (currencyNumber == 7) {myKey = keysWallet[msg.sender].currency7;}
-			if (currencyNumber == 8) {myKey = keysWallet[msg.sender].currency8;}
-			if (currencyNumber == 9) {myKey = keysWallet[msg.sender].currency9;}
+	function getKeyforCurrencyforID (bytes32 mySystemID, bytes32 payeeSystemID, uint currencyNumber) returns (bytes32 hisKey) {
+		if(keysWallet[mySystemID].ethereum == msg.sender) {
+			if (currencyNumber == 0) {hisKey = keysWallet[payeeSystemID].currency0;}
+			if (currencyNumber == 1) {hisKey = keysWallet[payeeSystemID].currency1;}
+			if (currencyNumber == 2) {hisKey = keysWallet[payeeSystemID].currency2;}
+			if (currencyNumber == 3) {hisKey = keysWallet[payeeSystemID].currency3;}
+			if (currencyNumber == 4) {hisKey = keysWallet[payeeSystemID].currency4;}
+			if (currencyNumber == 5) {hisKey = keysWallet[payeeSystemID].currency5;}
+			if (currencyNumber == 6) {hisKey = keysWallet[payeeSystemID].currency6;}
+			if (currencyNumber == 7) {hisKey = keysWallet[payeeSystemID].currency7;}
+			if (currencyNumber == 8) {hisKey = keysWallet[payeeSystemID].currency8;}
+			if (currencyNumber == 9) {hisKey = keysWallet[payeeSystemID].currency9;}
+			if (currencyNumber == 10) {hisKey = keysWallet[payeeSystemID].ethereum;}
 			}
-		}
-
-	function getEthAddress (bytes32 username) returns (address ethAddress) {
-		ethAddress = keysWallet[msg.sender].eID;
 		}
 }
 
